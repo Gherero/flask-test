060620b91a902fa724f0ec10dd3da158c366e6fd
gherero <katerina18>
2015-07-25 02:18:52 +0300

    1

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,7 @@ __author__ = 'gherero'
 from flask import render_template
 from app import app
 from .forms import LoginForm
+from .forms import NameForm
 from flask import request
 
 
@@ -9,11 +10,18 @@ from flask import request
 @app.route("/log", methods=['GET','POST'])
 def hello():
     form = LoginForm()
-    surname = form.surname.data
-    name = form.name.data
-    meddle_name=form.Middle_name.data
-    internet=form.internet.data
-    mail=form.mail.data
-    delo=form.delo.data
-    print (name)
-    return render_template('test.html', form=form, result=1)
+    name=form.name.data
+    login=form.login.data
+    passwd=form.passwrd.data
+    print (login)
+    return render_template('test.html', form=form)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        print(name)
+    return render_template('test.html', form=form, name=name)
--- app/forms.py
+++ app/forms.py
@@ -3,13 +3,34 @@ from flask_wtf import Form
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
+from flask.ext.wtf import Form
+from wtforms import TextField, PasswordField, validators, HiddenField
+from wtforms import TextAreaField, BooleanField
+from wtforms.validators import Required, EqualTo, Optional
+from wtforms.validators import Length, Email
+
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField()
+    submit = SubmitField()
+
 class LoginForm(Form):
-    surname = StringField('Фамилия:', validators=[DataRequired()])
+
+    login=StringField(validators=[DataRequired()])
+    passwrd=StringField()
+    #surname = StringField('Фамилия:', validators=[DataRequired()])
     name = StringField('Имя:', validators=[DataRequired()])
-    Middle_name = StringField('Отчество:', validators=[DataRequired()])
-    internet = BooleanField('Интернет', default=False)
-    mail=BooleanField("Почта",default=False)
-    delo=BooleanField("Дело",default=False)
+    #Middle_name = StringField('Отчество:', validators=[DataRequired()])
+    #internet = BooleanField('Интернет', default=False)
+    #mail=BooleanField("Почта",default=False)
+    #delo=BooleanField("Дело",default=False)
     #=BooleanField("",default=False)
+    email = TextField('Email address', validators=[
+           Required('Please provide a valid email address'),
+           Length(min=6, message=(u'Email address too short')),
+           Email(message=(u'That\'s not a valid email address.'))])
 
     print("wine")
