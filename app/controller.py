c7f9345085da4a79caaedcc624481c1b807ab10f
gherero <katerina18>
2015-07-26 03:26:54 +0300

    1

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,5 @@
 __author__ = 'gherero'
-from flask import render_template
+from flask import render_template, session,redirect,url_for
 from app import app
 from .forms import LoginForm
 from .forms import NameForm
@@ -9,19 +9,17 @@ from flask import request
 
 @app.route("/log", methods=['GET','POST'])
 def hello():
-    form = LoginForm()
-    name=form.name.data
-    login=form.login.data
-    passwd=form.passwrd.data
-    print (login)
-    return render_template('test.html', form=form)
+    #form = LoginForm()
+    #name=request.form['login']
+    #login=form.login.data
+    #passwd=form.passwrd.data
+    if request.method == 'POST':
+        print(request.form['username'])
+    #return 'login'
+    return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-        print(name)
-    return render_template('test.html', form=form, name=name)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form['username'])
+    return render_template('test.html')
